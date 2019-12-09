a6892701cf30b71773afa797a95396302ee17e43
taftadjani <taftadjani@gmail.com>
2019-12-09 17:31:38 +0100

    controller 2

--- routes/web.php
+++ routes/web.php
@@ -12,18 +12,18 @@
 */
 
 Route::get('/', 'WelcomeController@index');
-Route::get('/', 'WelcomeController@ajout_client');
-Route::get('/', 'WelcomeController@liste_clients');
-Route::get('/', 'WelcomeController@ajout_fournisseur');
-Route::get('/', 'WelcomeController@liste_fournisseurs');
-Route::get('/', 'WelcomeController@ajout_devis');
-Route::get('/', 'WelcomeController@liste_devis');
-Route::get('/', 'WelcomeController@ajout_bl');
-Route::get('/', 'WelcomeController@liste_bl');
-Route::get('/', 'WelcomeController@ajout_avoir');
-Route::get('/', 'WelcomeController@liste_avoirs');
-Route::get('/', 'WelcomeController@ajout_factures');
-Route::get('/', 'WelcomeController@liste_factures');
+Route::get('/ajout_client', 'WelcomeController@ajout_client');
+Route::get('/liste_clients', 'WelcomeController@liste_clients');
+Route::get('/ajout_fournisseur', 'WelcomeController@ajout_fournisseur');
+Route::get('/liste_fournisseurs', 'WelcomeController@liste_fournisseurs');
+Route::get('/ajout_devis', 'WelcomeController@ajout_devis');
+Route::get('/liste_devis', 'WelcomeController@liste_devis');
+Route::get('/ajout_bl', 'WelcomeController@ajout_bl');
+Route::get('/liste_bl', 'WelcomeController@liste_bl');
+Route::get('/ajout_avoir', 'WelcomeController@ajout_avoir');
+Route::get('/liste_avoirs', 'WelcomeController@liste_avoirs');
+Route::get('/ajout_factures', 'WelcomeController@ajout_factures');
+Route::get('/liste_factures', 'WelcomeController@liste_factures');
 Route::resource('Client', 'ClientController');
 Route::resource('Fournisseur', 'FournisseurController');
 Route::resource('Facture', 'FactureController');
